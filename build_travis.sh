853bc1ec102947eeb5f11d0f4762907ec33a627c
Eric Swann <eric.swann@q2ebanking.com>
2017-05-12 20:45:50 -0500

    Updated directory naming to cars

--- build_travis.sh
+++ build_travis.sh
@@ -47,7 +47,7 @@ if [ ! -d "$SCRIPT_DIR/.dotnet" ]; then
   mkdir "$SCRIPT_DIR/.dotnet"
 fi
 
-curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/v1.0.3/scripts/obtain/dotnet-install.sh
+curl -Lsfo "$SCRIPT_DIR/.dotnet/dotnet-install.sh" https://raw.githubusercontent.com/dotnet/cli/v1.0.4/scripts/obtain/dotnet-install.sh
 
 sudo bash "$SCRIPT_DIR/.dotnet/dotnet-install.sh" --version 1.0.4 --install-dir .dotnet --no-path
 export PATH="$SCRIPT_DIR/.dotnet":$PATH
